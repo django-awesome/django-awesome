de28f816413540ab2312109cf8ece82e3212d916
Bin <tu.nguyen@risotech.vn>
2024-01-04 00:08:58 +0700

    update allauth

---
+++ one/libraries/allauth/forms.py
@@ -0,0 +1,21 @@
+from allauth.account.forms import SignupForm
+from allauth.socialaccount.forms import SignupForm as SocialSignupForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class UserSignupForm(SignupForm):
+    """
+    Form that will be rendered on a user sign up section/screen.
+    Default fields will be added automatically.
+    Check UserSocialSignupForm for accounts created from social.
+    """
+
+
+class UserSocialSignupForm(SocialSignupForm):
+    """
+    Renders the form when user has signed up using social accounts.
+    Default fields will be added automatically.
+    See UserSignupForm otherwise.
+    """
