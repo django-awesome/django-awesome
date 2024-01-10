7a5c3af8c5ae89ab86dba8d8befd7367aa543b05
Bin <tu.nguyen@risotech.vn>
2024-01-10 12:09:02 +0700

    Update admin tool and oswald theme

--- config/settings/base.py
+++ config/settings/base.py
@@ -62,6 +62,9 @@ WSGI_APPLICATION = "config.wsgi.application"
 DJANGO_APPS = [
     "themes.oswald",
     "themes.oswald.allauth_ui",
+    "themes.oswald.admin_ui",
+    "one.contrib.admin.menu",
+    "one.libraries.grappelli",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
